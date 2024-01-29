8bb53155d24945fa7bd6ebbbed7e7a8fdb80ef28
Patrick Stünkel <webminz@googlemail.com>
2024-01-29 16:21:02 +0100

    demo house starter

---
+++ tests/demo_house.py
@@ -0,0 +1,9 @@
+from smarthouse.domain import SmartHouse
+
+DEMO_HOUSE = SmartHouse()
+
+# Building house structure
+ground_floor = DEMO_HOUSE.register_floor(1)
+entrance = DEMO_HOUSE.register_room(ground_floor, 13.5, "Entrance")
+# TODO: continue registering the remaining floor, rooms and devices
+
