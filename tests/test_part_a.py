5020c34c634f8b4fa7b14889907b7792e2f555f6
Patrick Stünkel <webminz@googlemail.com>
2025-02-24 21:05:33 +0100

    Pythonpaths...

--- tests/test_part_a.py
+++ tests/test_part_a.py
@@ -1,4 +1,9 @@
 from unittest import TestCase, main
+
+from pathlib import Path
+import sys 
+sys.path.append(str(Path().parent.absolute()))
+
 from smarthouse.domain import SmartHouse
 from demo_house import DEMO_HOUSE as h
 
